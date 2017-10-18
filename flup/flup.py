e256a9707bb8069cb43dd4f9f25bb6820a376c40
Angel Gomez <jangelgr7@gmail.com>
2017-10-18 19:02:41 -0400

    Import and use database file in Flask application

--- flup/flup.py
+++ flup/flup.py
@@ -1,6 +1,8 @@
 '''Main Flask application file.'''
 
-from flask import Flask
+from flask import Flask, g
+
+from .db import init_db
 
 
 def create_app(config=None):
@@ -13,4 +15,30 @@ def create_app(config=None):
     app.config.update(config or {})
     app.config.from_envvar('FLUP_SETTINGS', silent=True)
 
+    register_cli(app)
+    register_teardowns(app)
+
     return app
+
+
+def register_cli(app):
+    @app.cli.command('initdb')
+    def initdb_command():
+        '''
+        Initialise the database via command line, usage:
+            $ flask initdb
+        '''
+
+        init_db()
+        print('Initialised the database.')
+
+
+def register_teardowns(app):
+    @app.teardown_appcontext
+    def close_db(error):
+        '''
+        Closes the database connection at the end of the request.
+        '''
+
+        if hasattr(g, 'sqlite_db'):
+            g.sqlite_db.close()
