653c5b066c60acd1573e72cabc98e0d240505a8d
Angel Gomez <jangelgr7@gmail.com>
2017-11-08 21:36:08 -0400

    Add base test file

---
+++ tests/test_flup.py
@@ -0,0 +1,37 @@
+'''Tests the Flup application.'''
+
+import os
+import tempfile
+import pytest
+
+from flup.flup import create_app
+from flup.db import init_db
+
+
+@pytest.fixture
+def app(request):
+    db_fd, temp_db_location = tempfile.mkstemp()
+    config = {
+        'DATABASE': temp_db_location,
+        'TESTING': True,
+        'DB_FD': db_fd,
+    }
+
+    app = create_app(config=config)
+
+    with app.app_context():
+        init_db()
+        yield app
+
+
+@pytest.fixture
+def client(request, app):
+    client = app.test_client()
+
+    def teardown():
+        os.close(app.config['DB_FD'])
+        os.unlink(app.config['DATABASE'])
+
+    request.addfinalizer(teardown)
+
+    return client
